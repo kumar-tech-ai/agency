81b3da31d40beab47fdd3ad48ebcaf8f42be3e06
kumar-tech-ai <75980315+kumar-tech-ai@users.noreply.github.com>
2021-08-16 11:55:00 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Header from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -37,7 +38,7 @@ const Layout = ({ children }) => {
        
       </div>
     </>
-
+  <footer>
   )
 }
 
