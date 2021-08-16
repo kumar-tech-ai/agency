c0b218ce70b65b6f5fd16c79433a933a05247fa6
kumar-tech-ai <75980315+kumar-tech-ai@users.noreply.github.com>
2021-08-16 11:51:13 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,1 +1,14 @@
-
+ <footer
+          style={{
+          background:'#ffdd00',
+          }}
+        >
+          <div style={{
+          margin: `0 auto`,
+          padding: `0 1.0875rem 1.45rem`,
+          maxWidth: 1170}}>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.origamicreative.com">Origamicreative</a>
+        </div>
+        </footer>
