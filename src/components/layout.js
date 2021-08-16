392e1376c89bbd114a075510c7c11689d51ea90a
kumar-tech-ai <75980315+kumar-tech-ai@users.noreply.github.com>
2021-08-16 11:50:04 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,20 +37,7 @@ const Layout = ({ children }) => {
        
       </div>
     </>
- <footer
-          style={{
-          background:'#ffdd00',
-          }}
-        >
-          <div style={{
-          margin: `0 auto`,
-          padding: `0 1.0875rem 1.45rem`,
-          maxWidth: 1170}}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.origamicreative.com">Origamicreative</a>
-        </div>
-        </footer>
+
   )
 }
 
